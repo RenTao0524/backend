4fdb357a37cad91e326e23ad43ebfcd7a4528d4a
RenTao0524 <912683552@qq.com>
2022-12-02 13:35:39 +0800

    connect local mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserInfoController } from './user-info/user-info.controller';
 import { OrderController } from './order/order.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'rentao@1638',
+      database: 'userinfo',
+      // autoLoadEntities: true,
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController, UserInfoController, OrderController],
   providers: [AppService],
 })
